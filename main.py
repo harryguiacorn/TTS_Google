498e9d92aaf595b973bc2f69a9f9c3ec887d7426
Harry Gui <fxacorn@gmail.com>
2023-03-04 15:00:05 +0000

    updated

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-import tts_google
+import tts_google as tts
+
+tts.main("texts/Chapter 1 - LIVERMORE SPEAKS.txt",
+         "audio/Chapter 1 - LIVERMORE SPEAKS.mp3")
--- tts_google.py
+++ tts_google.py
@@ -1,17 +1,19 @@
 import gtts
-# from playsound import playsound
 
-with open('Chapter 1 - LIVERMORE SPEAKS.txt') as f:
-  lines = f.read()
-  # print(lines)
 
-  # Create a gTTS object and set the language to English (en) and gender to female (f)
-  tts = gtts.gTTS(text=lines, lang='en', tld='co.uk', slow=False)
+def main(__importTextPath="", __exportAudioPath=""):
+  print("here: ", __importTextPath)
+  print("here: ", __exportAudioPath)
+  with open(__importTextPath) as f:
+    lines = f.read()
+    # print(lines)
 
-  # save the audio file
-  tts.save("Chapter 1 - LIVERMORE SPEAKS.mp3")
-  # play the audio file
-  # playsound("hello.mp3")
+    # Create a gTTS object and set the language to English (en) and gender to female (f)
+    # tts = gtts.gTTS(text=lines, lang='en', tld='co.uk', slow=False)
 
-  # all available languages along with their IETF tag
-  # print(gtts.lang.tts_langs())
+    # save the audio file
+    # tts.save(__exportAudioPath)
+
+
+if __name__ == "__main__":
+  main()
